f627f1ad0953946bcc05eae43d1d69fbd248296e
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-08-06 14:35:37 +0530

    06.08.25 complete

--- src/main/java/loginpage/LoginPage.java
+++ src/main/java/loginpage/LoginPage.java
@@ -1,4 +1,30 @@
 package loginpage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    WebDriver driver;
+
+    @FindBy(id = "username")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "loginBtn")
+    WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password) {
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
 }
--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -1,4 +1,38 @@
 package loginTest;
 
+import loginpage.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class LoginTest {
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://example.com/login");
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void testValidLogin() {
+        loginPage.login("testuser", "testpass");
+        WebElement dashboard = driver.findElement(By.id("dashboard"));
+        Assert.assertTrue(dashboard.isDisplayed(), "Login failed or dashboard not visible");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
